ffd3c6fb2f466f3d9c51994fd846227acacf9132
vbochagin112-png <vbochagin112@gmail.com>
2026-02-12 18:23:12 +0300

    Add files via upload

---
+++ Урок 2 JS/script.js
@@ -0,0 +1,28 @@
+// Задание 1
+console.log ('7') 
+
+
+// Задание 2
+console.log ('Добро ' + 'пожаловать ' + 'на курс ' + 'Владлен')
+
+
+// Задание 3
+
+// const Text = ('12.02.2026'); 
+// alert(Text)
+
+
+
+// Задание 5
+
+const myAge = 12
+const myName = 'Влад'
+const myFirstName = true
+const myAnimal = null
+let myFlower
+ 
+console.log (typeof myAge)
+console.log (typeof myName)
+console.log (typeof myFirstName)
+console.log (typeof myAnimal)
+console.log (typeof myFlower)
