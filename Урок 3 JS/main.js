0f4775c5cdbeaf4c69bffc989a5f4f9792947d4f
vbochagin112-png <vbochagin112@gmail.com>
2026-02-21 21:36:56 +0300

    Add files via upload

---
+++ Урок 3 JS/main.js
@@ -0,0 +1,20 @@
+
+let randomNum = prompt ('Введи любое число. Так надо!')
+let div = document.querySelector ('.block')
+
+if (randomNum > 100) {
+    let e = randomNum
+    div.innerHTML = e 
+} else {
+    console.log (randomNum)
+}
+
+
+
+
+
+
+
+
+
+
