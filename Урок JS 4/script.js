7159c0db84e854a38ebc377f6eb886e87222929a
vbochagin112-png <vbochagin112@gmail.com>
2026-02-22 18:34:31 +0300

    Add files via upload

---
+++ Урок JS 4/script.js
@@ -0,0 +1,38 @@
+// let ask = prompt ('Сколько ты видишь пальцев у себя на руке?')
+// let div = document.querySelector ('.block')
+
+// if (ask == 5) {
+//     let e = ask
+//     div.innerHTML = 'OOOOOOOOOOOO!'
+// } else {
+//     alert ('lol')
+// }
+
+
+
+// Домашнее задание
+let a = 5
+let b = 5
+
+
+// console.log (a + b == 5 || b == 15)
+
+
+
+if (a + b > 15) {
+    console.log ('ДА')
+} else {
+    console.log ('НЕТ')
+}
+    
+
+
+
+if (a == b) {
+    console.log (true)
+} else {
+    console.log (false)
+}
+
+
+
