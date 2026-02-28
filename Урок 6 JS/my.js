510326951996c0d6ad746c4b80afce0c24c3a35d
vbochagin112-png <vbochagin112@gmail.com>
2026-02-28 13:34:40 +0300

    Add files via upload

---
+++ Урок 6 JS/my.js
@@ -0,0 +1,77 @@
+// let x = 25
+
+// switch (x) {
+//     case 25:
+//     console.log (true)
+//     break
+//      case 15:
+//     console.log(false)
+// }
+
+
+// let i = 10
+
+// while (i <= 50) {
+//  console.log (i)
+//  i++
+// }
+
+// let t = 25
+
+// do {
+//     console.log (t)
+//     t--
+// } while (t >= 10) {
+// }
+
+// for (let e = 10; e <= 25; e++ ) {
+//     console.log (e)
+// }
+    
+
+//Домашнее задание 
+
+let ageVlad = 25
+let ageKolyan = +prompt('Сколько лет Коляну?')
+
+
+
+switch (ageVlad === ageKolyan) {
+case true:
+    console.log('Да')
+    break
+    case false:
+    console.log ('Нет')
+    break
+    default:
+        console.log ('Не получилось')
+}
+
+
+let a = 13
+
+switch (a) {
+    case 1:
+    case 2:
+    case 3:
+        console.log ('Зима')
+    break
+    case 4:
+    case 5:
+    case 6:
+        console.log ('Весна')
+    break
+    case 7:
+    case 8:
+    case 9:
+        console.log ('Лето')
+    break
+    case 10:
+    case 11:
+    case 12:
+        console.log ('Осень')
+    break
+    default:
+        console.log ('Не получилось')
+    
+}
